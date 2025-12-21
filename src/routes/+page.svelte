b185bbfe0779c88540b727e061ae8299129a9159
Samuel Ratnam <samueljratnam@gmail.com>
2025-12-21 18:42:47 +0000

    v0

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<svelte:head>
+    <title>The Idealists Collective</title>
+</svelte:head>
+
+<h1>THE IDEALISTS COLLECTIVE</h1>
+<br>
+<div>
+    <h2>our dna</h2>
+    <ul>
+        <li>
+            <h3>autonomous</h3>
+            <p>bla bla</p>
+        </li>
+        <li>
+            <h3>living</h3>
+            <p>we are rhizomatic</p>
+        </li>
+        <li>
+            <h3>playful</h3>
+            <p>whimsey and joy is integral to imaginining the ideal</p>
+        </li>
+        <li>
+            <h3>cooperative</h3>
+            <p>something</p>
+        </li>
+        <li>
+            <h3>utopian</h3>
+            <p>our idealist ethos factors into 3 oppositions:</p>
+        </li>
+        <li>
+            <h3>love</h3>
+            <p>don't compromise on your integrity. do everything with love.</p>
+        </li>
+    </ul>
+</div>
